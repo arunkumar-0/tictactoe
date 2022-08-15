e3ba071c996900a84abfd3dab660835a9d86257c
Your Name <you@example.com>
2022-08-15 12:48:30 +0530

    added board state and ae & react events

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 
 
-const Square = (props) => {
-    console.log(props);
-
-  return <button type="button" className='square'> {props.value} </button>;
+const Square = ({value , onClick}) => {
+  return ( 
+ <button
+  type="button"  className='square' onClick={onClick}>
+     {value} </button>
+  );
  
 };
 
